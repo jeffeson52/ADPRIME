5c7cd10d7066fbea3c76d2e43fbd997ed8510193
Jeffeson Augusto <31020056+jeffeson52@users.noreply.github.com>
2020-05-25 16:05:49 -0300

    Add files via upload

---
+++ tela_usuario/cadastrarUsuario.php
@@ -0,0 +1,128 @@
+<?php
+require_once '../tela_login/usuarios.php';
+$u = new Usuario;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+
+    <title>Cadastrar novo usuário</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/cadastrar.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../js/arquivo.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+</head>
+
+<body>
+    <div class="preencher" style="margin-top:100px;">&nbsp;</div>
+    <div class="login">
+        <div class="voltar">
+            <a href="./administrador.php">
+                <h1><i class="fas fa-sign-out-alt"></i></h1>
+            </a>
+            <p>Voltar</p>
+        </div>
+        <h1>Cadastrar</h1>
+
+        <form method="POST">
+            <!-- não apaga sa porra GABRIEL -->
+            <br>
+            <p>Nome</p>
+            <a class="popupbt"><i class="far fa-user"></i></a>&nbsp;&nbsp;<input type="text" id="name" name="nome" placeholder="Insira seu nome" maxlength="40" />
+            <p>E-mail</p>
+            <a class="popupbt"><i class="fas fa-envelope"></i></a>&nbsp;&nbsp;<input type="text" id="name" name="email" placeholder="Insira seu email" maxlength="30" />
+            <p>Telefone</p>
+            <a class="popupbt"><i class="fas fa-phone"></i></a>&nbsp;&nbsp;<input type="text" id="name" name="telefone" placeholder="Insira seu telefone " maxlength="15" />
+            <p>Senha</p>
+            <a class="popupbt"><i class="fa fa-lock"></i></a>&nbsp;&nbsp;<input type="password" id="senha" name="senha" placeholder="Insira sua senha" maxlength="15" />
+            <button id="mostrarsenha" type="button" onclick="mostrarSenha()" style="position: absolute;font-size:1.3em;"><i class="far fa-eye"></i></button>
+            <p>Confirma Senha</p>
+            <a class="popupbt"><i class="fas fa-lock"></i></a>&nbsp;&nbsp;<input type="password" id="senha2" name="Csenha" placeholder="Confirme sua senha" maxlength="15" />
+
+            <select name="nivel"  style="background-color:inherit; color:dodgerblue; border-color:#555555; font-weight:400; margin-bottom: 20px;">
+                <option value="">Selecione</option>
+                <option value="2">Usuario Comum</option>
+                <option value="1">Administrador</option>
+            </select><br />
+
+            <input type="submit" id="enviar" value="Cadastrar"><br>
+
+            <?php
+            if (isset($_POST['nome'])) {
+                $nome = addslashes($_POST['nome']);
+                $telefone = addslashes($_POST['telefone']);
+                $email = addslashes($_POST['email']);
+                $senha = addslashes($_POST['senha']);
+                $Csenha = addslashes($_POST['Csenha']);
+                $nivel = addslashes($_POST['nivel']);
+
+                //verifica se está tudo preenchido
+                if (!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($Csenha) && !empty($nivel)) {
+                    $u->conectar("adprime2", "localhost", "root", "");
+                    if ($u->msgErro == "") {
+                        if ($senha == $Csenha) {
+                            if ($u->cadastrar($nome, $telefone, $email, $senha, $nivel)) {
+            ?>
+                                <!--Nesta parte tudo é HTML, o PHP em nada interfere, você pode fazer seu CSS normalmente, apenas nas DIV-->
+                                <div style=" border-radius:10px; margin: 10px auto; padding: 10px; background-color: rgba(50, 205, 50, .2); border: 1px solid rgb(34, 139,34);">
+                                    Cadastrado com sucesso
+                                </div>
+                            <?php
+                            } else {
+                            ?>
+                                <div style="border-radius:10px; margin: 10px auto; padding: 10px; background-color: rgba(250, 128, 114, .2); border: 1px solid rgb(165, 42,42);">
+                                    Email já cadastrado
+                                </div>
+                            <?php
+                            }
+                        } else {
+                            ?>
+                            <div style="border-radius:10px; margin: 10px auto; padding: 10px; background-color: rgba(250, 128, 114, .2); border: 1px solid rgb(165, 42,42);">
+                                As senhas não correspondem
+                            </div>
+                        <?php
+                        }
+                    } else {
+                        ?>
+                        <div style="border-radius:10px; margin: 10px auto; padding: 10px; background-color: rgba(250, 128, 114, .2); border: 1px solid rgb(165, 42,42);">
+                            <?php echo "Erro: " . $u->msgErro; ?>
+                        </div>
+                    <?php
+                    }
+                } else {
+                    ?>
+                    <div style="border-radius:10px; margin: 10px auto; padding: 10px; background-color: rgba(250, 128, 114, .2); border: 1px solid rgb(165, 42,42);">
+                        Preencha todos os Campos
+                    </div>
+            <?php
+                }
+            }
+            ?>
+
+        </form>
+    </div>
+    <script>
+        function mostrarSenha() {
+            var tipo = document.getElementById("senha");
+            if (tipo.type == "password") {
+                tipo.type = "text";
+            } else {
+                tipo.type = "password";
+            }
+
+            var tipo2 = document.getElementById("senha2");
+            if (tipo2.type == "password") {
+                tipo2.type = "text";
+            } else {
+                tipo2.type = "password";
+            }
+        }
+    </script>
+
+</body>
+
+</html>
